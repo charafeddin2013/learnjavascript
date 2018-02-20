74300086346dbbd57a289f292036df7918cc1ccf
charafeddin2013 <charafeddin2013@gmail.com>
2018-02-20 13:20:59 +0100

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+//We donot need to declare the variable type
+
+//declare number variables
+var number=10;
+document.write("my number is"+number);
+
+//declare booleans
+var bool=true;
+document.write("i am a boolean variable and my value is " + bool);
+
+// create strings
+var greetings = "Hello world";
+
+//string length
+var len=greetings.length;
+document.write("The length of \""+ greetings +"\" is" +len);
+
+// get the type of variables
+document.write(typeof number + " " +typeof bool + " " +typeof greetings);
