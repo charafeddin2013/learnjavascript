d801bb2a4c22af549e655d4dea64754fb98d7cc9
charafeddin2013 <charafeddin2013@gmail.com>
2018-02-20 14:02:57 +0100

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -2,18 +2,18 @@
 
 //declare number variables
 var number=10;
-document.write("my number is"+number);
+console.log("my number is "+number);
 
 //declare booleans
 var bool=true;
-document.write("i am a boolean variable and my value is " + bool);
+console.log("i am a boolean variable and my value is " + bool);
 
 // create strings
 var greetings = "Hello world";
 
 //string length
 var len=greetings.length;
-document.write("The length of \""+ greetings +"\" is" +len);
+console.log("The length of \""+ greetings +"\" is: " +len);
 
 // get the type of variables
-document.write(typeof number + " " +typeof bool + " " +typeof greetings);
+console.log(typeof number + " " +typeof bool + " " +typeof greetings);
